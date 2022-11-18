50b14bb814716fe0d1ff41d98aecd90efa22e6b0
RyanEweSeng <ryantoh1001@gmail.com>
2022-11-18 13:35:46 -0600

    Implement manhattan distance function

--- hw8-puzzle/funny_puzzle.py
+++ hw8-puzzle/funny_puzzle.py
@@ -1,21 +1,29 @@
 import heapq
 
 
-
-
 def get_manhattan_distance(from_state, to_state=[1, 2, 3, 4, 5, 6, 7, 0, 0]):
     """
-    TODO: implement this function. This function will not be tested directly by the grader. 
-
     INPUT: 
         Two states (if second state is omitted then it is assumed that it is the goal state)
 
     RETURNS:
         A scalar that is the sum of Manhattan distances for all tiles.
     """
+    width = 3
+    distance = x1 = y1 = x2 = y2 = 0
+    for num in range(1,8):
+        for r in range(3):
+            for c in range(3):
+                idx = width * r + c
+                if from_state[idx] == num:
+                    x1 = c
+                    y1 = r
+                if to_state[idx] == num:
+                    x2 = c
+                    y2 = r
+        distance += abs(x1-x2) + abs(y1-y2)
+
     return distance
-
-
 
 
 def print_succ(state):
@@ -64,11 +72,11 @@ if __name__ == "__main__":
     Feel free to write your own test code here to exaime the correctness of your functions. 
     Note that this part will not be graded.
     """
-    print_succ([2,5,1,4,0,6,7,0,3])
-    print()
+    # print_succ([2,5,1,4,0,6,7,0,3])
+    # print()
 
     print(get_manhattan_distance([2,5,1,4,0,6,7,0,3], [1, 2, 3, 4, 5, 6, 7, 0, 0]))
     print()
 
-    solve([2,5,1,4,0,6,7,0,3])
-    print()
+    # solve([2,5,1,4,0,6,7,0,3])
+    # print()
